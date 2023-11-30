2aa6ca5edbf7d515c52ad27e0252a396ac444a9a
pereirax <142833399+pereirax@users.noreply.github.com>
2023-11-30 17:16:11 -0300

    Create calculadoraSimples.py
    Thirt

---
+++ calculadoraSimples.py
@@ -0,0 +1,7 @@
+#n1=input("Digite o primeiro número : ")
+#n2=input("Digite o segundo número : ")
+#print(int(n1)+(n2))
+
+n1 = int(input("Digite o primeiro numero :"))
+n2 = int(input("Digite o segundo numero :"))
+print(n1+n2)
