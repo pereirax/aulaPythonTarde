db2b8dfa15e566c1421bb19549eded74cbcdda00
pereirax <142833399+pereirax@users.noreply.github.com>
2023-11-30 15:17:24 -0300

    Create helloword.py
    first

---
+++ helloword.py
@@ -0,0 +1,9 @@
+print("HelloWord !") # Mostrar na tela o texto Helloword!
+mensagem = "Ola, tudo bem?"
+print(mensagem)#mostra a mensagem na tela.
+print(mensagem[1])# mostra a segunda letra da mensagem.
+print(mensagem[0:4])# mostra da primeira a quarta letra da mensagem.
+print(mensagem[-1])# mostra a ultima letra da imagem.
+mensagem = "Ola, tudo bem?"
+print(mensagem[0:4])
+print(mensagem[4::]) # mostra da quarta posição até o fim.
